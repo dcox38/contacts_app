7a9b4adc6972959bfc7d613fed1d08633ef8c98c
Dan <dcox38@gmailL.com>
2020-07-05 12:14:09 -0500

    blank slate

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,7 +1,4 @@
 class Api::ContactsController < ApplicationController
-  def the_contact
-    @contact = Contact.first
-    render 'contact.json.jb'
-  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/first_contact" => "contacts#the_contact"
+
+
   end
 end
