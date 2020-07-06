f97a856cf558e493e043cc71119ce9e0debd55b3
Dan <dcox38@gmailL.com>
2020-07-06 18:35:38 -0500

    added create and show routes and methods

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -6,4 +6,23 @@ class Api::ContactsController < ApplicationController
     render 'index.json.jb'
   end
 
+  def create
+    @contact = Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+
+    render 'show.json.jb'
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+
+    render "show.json.jb"
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
 
     get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
+    get "/contacts/:id" => "contacts#show"
 
 
   end
