c786e1e4ae4c04e4bf0072f6e374f217fb131874
Dan <dcox38@gmailL.com>
2020-07-06 18:49:28 -0500

    add update and destroy routes and methods

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -19,9 +19,31 @@ class Api::ContactsController < ApplicationController
 
   def show
     @contact = Contact.find_by(id: params[:id])
+    
+    render "show.json.jb"
+
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
 
     render "show.json.jb"
 
+  end
+
+  def destroy
+
+    @contact = Contact.find_by(id: params[:id])    
+    @contact.destroy
+
+    render json: {message: "Great job, you have deleted a contact!"}
   end
 
 
